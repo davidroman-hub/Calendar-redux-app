9fa6f254bc39395dc2d802eaaf33dab6433d6af9
David <jobroman83@gmail.com>
2020-10-07 16:49:48 +0200

    task 1 starting the mew project , routing the pages

---
+++ src/CalendarApp.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import {AppRouter} from './router/AppRouter'
+
+
+export const CalendarApp = () => {
+    return (
+        <div>
+            <AppRouter/>
+        </div>
+    )
+}
+
+export default CalendarApp
+
+//rafc
