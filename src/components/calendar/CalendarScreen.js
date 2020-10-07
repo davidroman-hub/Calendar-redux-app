f8263ec5bcc558a1f9f900a43a4e6e6f07a536b3
David <jobroman83@gmail.com>
2020-10-07 17:20:05 +0200

    task 2 creating calenadar component

--- src/components/calendar/CalendarScreen.js
+++ src/components/calendar/CalendarScreen.js
@@ -1,9 +1,31 @@
 import React from 'react'
+import {Calendar,  momentLocalizer} from 'react-big-calendar';
+import moment from 'moment';
+import Navbar from '../ui/Nabvar';
+import 'react-big-calendar/lib/css/react-big-calendar.css'
+import '../../../src/styles.css'
+const localizer = momentLocalizer(moment) // or globalizeLocalizer
+
+const events = [{
+    title:'Cumple del jefe',
+    start: moment().toDate(),
+    end: moment().add(2, 'hours').toDate(),
+    bgColor:'#fafafa'
+}]
 
 export const CalendarScreen = () => {
+    
+    
     return (
-        <div>
-            <h1>Calendar Screen</h1>
+        <div className='calendar-screen'>
+            <Navbar/>
+                <Calendar
+                    localizer={localizer}
+                    events={events}
+                    startAccessor="start"
+                    endAccessor="end"
+                />
+
         </div>
     )
 }
---
+++ src/components/ui/Nabvar.js
@@ -0,0 +1,13 @@
+
+import React from 'react'
+
+export const Nabvar = () => {
+    return (
+        <div>
+            <h1>Nabvar</h1>
+        </div>
+    )
+}
+
+
+export default Nabvar
